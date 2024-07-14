d24af555ec8bfcea356640f302c8db9719f63c8b
Israel14Ec <96362448+Israel14Ec@users.noreply.github.com>
2024-07-14 20:44:38 +0200

    Actualizado para websockets

--- src/config/echo.js
+++ src/config/echo.js
@@ -2,28 +2,17 @@ import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 
 window.Pusher = Pusher;
-
-const token = localStorage.getItem('AUTH_TOKEN');
-console.log(token);
-
 const echoConfig = {
     broadcaster: 'pusher',
-    key: 'miAppKeyProd',  // Este puede ser cualquier string ya que no estamos usando Pusher
+    key: 'miAppKeyProd', // Puede ser cualquier string ya que no estamos usando Pusher
     wsHost: 'xpressback-production.up.railway.app', // Apunta a tu dominio de WebSockets
     wsPort: 6001, // Puerto del servidor WebSockets
     wssPort: 6001, // Puerto seguro del servidor WebSockets
-    forceTLS: true, // Usa true si estás usando SSL, false de lo contrario
+    forceTLS: false, // Usa false si no estás usando SSL
     disableStats: true,
-    cluster: 'mt1', // Puedes omitir esto si no usas Pusher
     enabledTransports: ['ws', 'wss'], // Solo habilitar WebSocket
-    authEndpoint: 'https://xpressback-production.up.railway.app/broadcasting/auth', // Cambia a tu endpoint de autenticación
-    auth: {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    },
 };
 
-const echoInstance = new Echo(echoConfig);
+window.Echo = new Echo(echoConfig);
 
-export default echoInstance;
+export default window.Echo;
