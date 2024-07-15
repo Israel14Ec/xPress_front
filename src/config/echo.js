a6b1220b956f8a54728c03f1397daf6b54cddd6b
Israel14Ec <96362448+Israel14Ec@users.noreply.github.com>
2024-07-15 02:12:28 +0200

    Configuracion de echojs

--- src/config/echo.js
+++ src/config/echo.js
@@ -4,26 +4,25 @@ import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
 const token = localStorage.getItem('AUTH_TOKEN');
-console.log(token);
 
 const echoConfig = {
     broadcaster: 'pusher',
-    key: 'miAppKeyProd',  // Este puede ser cualquier string ya que no estamos usando Pusher
+    key: 'miAppKeyProd', // Puede ser cualquier string ya que no estamos usando Pusher
     wsHost: 'xpressback-production.up.railway.app', // Apunta a tu dominio de WebSockets
     wsPort: 6001, // Puerto del servidor WebSockets
-    wssPort: 6001, // Puerto seguro del servidor WebSockets
-    forceTLS: true, // Usa true si estás usando SSL, false de lo contrario
+    wssPort: 6001, // Puerto seguro del servidor WebSockets (puedes ignorarlo si no usas SSL)
+    forceTLS: false, // Asegúrate de que esté en false si no estás usando SSL
     disableStats: true,
-    cluster: 'mt1', // Puedes omitir esto si no usas Pusher
-    enabledTransports: ['ws', 'wss'], // Solo habilitar WebSocket
+    enabledTransports: ['ws','wss'], // Solo habilitar WebSocket sin SSL
+    cluster: 'mt1', // Asegúrate de incluir el cluster
     authEndpoint: 'https://xpressback-production.up.railway.app/broadcasting/auth', // Cambia a tu endpoint de autenticación
     auth: {
         headers: {
             Authorization: `Bearer ${token}`,
-        },
-    },
+        }
+    }
 };
 
-const echoInstance = new Echo(echoConfig);
+window.Echo = new Echo(echoConfig);
 
-export default echoInstance;
+export default window.Echo;
